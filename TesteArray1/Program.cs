a25e18453e248082fb231a611807b5460e3d5b8d
Usuario <Usuario@CPD02-M13>
2024-04-30 10:46:06 -0300

    Adicionar arquivos de projeto.

---
+++ TesteArray1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteArray1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = { "Adelson", "Alayoson", "Allyson", "Abebel", "Allylson", "Edson" };
+            Console.WriteLine("Digite um número de 0 a 6: ");
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O nome selecionado foi: " + nome[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
